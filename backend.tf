305708a7329b888344ea958066432ec05d6aa582
deekshameshram777 <156531844+deekshameshram777@users.noreply.github.com>
2024-06-20 16:10:36 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "test-rg"
-    storage_account_name = "teststorage123"
+    storage_account_name = "teststorage7771234"
     container_name       = "devtest-tfstate"
     key                  = "devtest.terraform.tfstate"
   }
