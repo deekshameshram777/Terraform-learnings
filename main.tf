fa80a27fdb5d70590ab5f0d8e2d4c5662818de05
deekshameshram777 <156531844+deekshameshram777@users.noreply.github.com>
2024-09-11 17:18:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "demorg" {
 }
 
 resource "azurerm_storage_account" "demostorage7799" {
-  name                     = "${var.prefix}storage7799"
+  name                     = "co${var.domain}us6${var.prefix}sac01"
   resource_group_name      = azurerm_resource_group.demorg.name
   location                 = var.location
   account_tier             = "Standard"
